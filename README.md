4ea0c60a0ac4857ee14ee860e0d317e045ea8984
Nadavc220 <nadavc220@gmail.com>
2025-06-04 21:30:01 +0300

    Updates

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# conditional-balance-analysis
+<h1 align="center">
+  [CVPR 25] Conditional Balance:  <br>
+  Improving Multi-Conditioning Trade-Offs in Image Generation <br>
+  Model Analysis Repository
+</h1>
+
+<p align='center'>
+<a href="https://nadavc220.github.io/conditional-balance.github.io/"><img src="https://img.shields.io/static/v1?label=Project&message=Website&color=blue"></a>
+<a href="https://www.youtube.com/watch?v=WNRcC-fLXq8"><img src="https://img.shields.io/static/v1?label=YouTube&message=Video&color=orange"></a>
+<a href="https://arxiv.org/abs/2412.19853"><img src="https://img.shields.io/badge/arXiv-2412.19853-b31b1b.svg"></a>
+<a href="https://pytorch.org/"><img src="https://img.shields.io/badge/PyTorch->=2.4.1-Red?logo=pytorch"></a>
+</p>
+
+
+This is the official repository of the model analysis method presented in our paper "Conditional Balance: Improving Multi-Conditioning Trade-Offs in Image Generation" by Nadav Z. Cohen, Oron Nir, and Ariel Shamir.
+
+If you are looking for the inference code, click the following link: [Inference Code](https://github.com/Nadavc220/conditional-balance).
+
+![teaser](assets/method_v2.jpg)
+
+
+# Environment Setup
+Please follow enviornemt setup as presented in our main repo: [Inference Code](https://github.com/Nadavc220/conditional-balance).
+
+
+# Model Analysis
+Every experiment runs two steps as presented in the paper:
+
+### 1) Image Series Generation
+The conditionals for the image series are defined in the analysis_files folder. We share an example for using the rabbit in the texture analysis and the cat/wolf in the geometry analysis. Each experiment uses it's own script as they run a bit differently.
+
+During this step, the self attention features are gathered for the next step.
+```
+### Texture Script
+python extract_layers_statistics.py --series_name style_analysis_texture_rabbit \
+                                    --content_file content_texture_rabbit.yaml \
+                                    --style_file style_prompts_texture.yaml 
+
+### Geometry Script
+python style_gen_img.py --series_name style_analysis_geometry_cat \
+                        --subject cat
+```
+
+### 2) Layer Grading
+After we gathered the layer statistics in step 1, it's time to grade their sensitivity. This can be done by calling the grade_layers_jsd.py file with out series name:
+
+```
+python grade_layers_jsd.py --series_name style_analysis_texture_rabbit
+```
+Note that this step is the same for both experiments, but you need to make sure you are using the same series name for both steps.
+
+### 3) Grading Combination (Optional)
+In our paper, we combined experiments gradings to get a more robust result. If you wish to do so too, you can use the "combine_grading.py" file.
+
+# Citation
+If you found this project helpful in your research, please consider citing our paper.
+```
+@InProceedings{Cohen_2025_CVPR,
+    author    = {Cohen, Nadav Z. and Nir, Oron and Shamir, Ariel},
+    title     = {Conditional Balance: Improving Multi-Conditioning Trade-Offs in Image Generation},
+    booktitle = {Proceedings of the Computer Vision and Pattern Recognition Conference (CVPR)},
+    month     = {June},
+    year      = {2025},
+    pages     = {2641-2650}
+}
+```
+```
+@misc{cohen2024conditionalbalanceimprovingmulticonditioning,
+      title={Conditional Balance: Improving Multi-Conditioning Trade-Offs in Image Generation}, 
+      author={Nadav Z. Cohen and Oron Nir and Ariel Shamir},
+      year={2024},
+      eprint={2412.19853},
+      archivePrefix={arXiv},
+      primaryClass={cs.CV},
+      url={https://arxiv.org/abs/2412.19853}, 
+}
+```
+
+# Acknowledgments
+We wish to thank the authors of [StyleAligned](https://github.com/google/style-aligned) as parts of our code were adopted from their repository.
--- extract_layers_statistics_geometry.py
+++ extract_layers_statistics_geometry.py
@@ -2,7 +2,8 @@
 This script extracts and records the adain features of generated images using style aligned.
 The output of this script can be used woth "grade_layers.py" to grade the layers for style balance.
 """
-
+import os
+import argparse
 from PIL import Image
 import torch
 
@@ -11,103 +12,109 @@ from diffusers import ControlNetModel, StableDiffusionXLControlNetPipeline, Auto
 from handlers import adain_handler_sa
 from models.sdxl_controlnet_stylealigned_analysis import StableDiffusionXLControlNetStyleAlignedPipeline
 
-import os
-
 from utils import prepare_canny_image
 
 THREE_CATS_CANNY_VALUES = (50, 100)
+SERIES_NAME = "style_analysis_geometry_cat"
+SUBJECT = "cat"
+
+def main(args):
+    input_dir = f'analysis_files/geometry/{args.subject}'
+    output_dir = f'outputs/statistics/{args.series_name}'
+    os.makedirs(output_dir, exist_ok=True)
+
+    output_image_dir = os.path.join(output_dir, 'images')
+    output_canny_dir = os.path.join(output_dir, 'cannies')
+    output_style_dir = os.path.join(output_dir, 'styles')
+    output_stats_dir = os.path.join(output_dir, 'stats')
+
+    os.makedirs(output_image_dir, exist_ok=True)
+    os.makedirs(output_canny_dir, exist_ok=True)
+    os.makedirs(output_style_dir, exist_ok=True)
+    os.makedirs(output_stats_dir, exist_ok=True)
+
+    #### Load DDPM'S ###########################################################
+    controlnet = ControlNetModel.from_pretrained(
+        "diffusers/controlnet-canny-sdxl-1.0",
+        variant="fp16",
+        use_safetensors=True,
+        torch_dtype=torch.float16
+    ).to("cuda")
+
+    # load both base & refiner
+    vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16).to("cuda")
+
+    pipeline = StableDiffusionXLControlNetPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-xl-base-1.0",
+        controlnet=controlnet,
+        vae=vae,
+        variant="fp16",
+        use_safetensors=True,
+        torch_dtype=torch.float16,
+        # scheduler=scheduler
+    ).to("cuda")
 
 
-#### Load DDPM'S ###########################################################
-controlnet = ControlNetModel.from_pretrained(
-    "diffusers/controlnet-canny-sdxl-1.0",
-    variant="fp16",
-    use_safetensors=True,
-    torch_dtype=torch.float16
-).to("cuda")
-
-# load both base & refiner
-vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16).to("cuda")
-
-pipeline = StableDiffusionXLControlNetPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-base-1.0",
-    controlnet=controlnet,
-    vae=vae,
-    variant="fp16",
-    use_safetensors=True,
-    torch_dtype=torch.float16,
-    # scheduler=scheduler
-).to("cuda")
-
-
-sa_args = adain_handler_sa.StyleAlignedArgs(share_group_norm=False,
-                                      share_layer_norm=False,
-                                      share_attention=True,
-                                      adain_queries=True,
-                                      adain_keys=True,
-                                      adain_values=False,
-                                     )
+    sa_args = adain_handler_sa.StyleAlignedArgs(share_group_norm=False,
+                                        share_layer_norm=False,
+                                        share_attention=True,
+                                        adain_queries=True,
+                                        adain_keys=True,
+                                        adain_values=False,
+                                        )
 
 
 
-model = StableDiffusionXLControlNetStyleAlignedPipeline(pipeline, sa_args)
+    model = StableDiffusionXLControlNetStyleAlignedPipeline(pipeline, sa_args)
 
-#### Prepare Inputs ###########################################################
-
-input_dir = 'analysis_files/geometry/cat'
-output_dir = 'outputs/statistics/style_analysis_geometry_cat'
-os.makedirs(output_dir, exist_ok=True)
-
-output_image_dir = os.path.join(output_dir, 'images')
-output_canny_dir = os.path.join(output_dir, 'cannies')
-output_style_dir = os.path.join(output_dir, 'styles')
-output_stats_dir = os.path.join(output_dir, 'stats')
-
-os.makedirs(output_image_dir, exist_ok=True)
-os.makedirs(output_canny_dir, exist_ok=True)
-os.makedirs(output_style_dir, exist_ok=True)
-os.makedirs(output_stats_dir, exist_ok=True)
-
-layer_names_file = os.path.join(output_dir, "layer_names")
-
-style_prompts = 'in line art style'
-reference_prompts = 'An ink painting'
-main_prompts = 'A photo of a sitting cat'
-
-target_prompt = f"{main_prompts}, {style_prompts}"
-reference_prompt = f"{reference_prompts}, {style_prompts}"
-prompts = [reference_prompt, target_prompt]
-
-files = os.listdir(input_dir)
-
-style_dict = {artist: sorted([f_name for f_name in files if artist in f_name]) for artist in set([f_name.split("_")[0] for f_name in files])}
-num_styles = len(style_dict.keys())
-num_per_style = len(list(style_dict.values())[0])
+    #### Prepare Inputs ###########################################################
 
 
-for i, k in enumerate(style_dict.keys()):
-    assert len(style_dict[k]) == num_per_style
-    for j, file in enumerate(style_dict[k]):
-        torch.manual_seed(10)
+    layer_names_file = os.path.join(output_dir, "layer_names")
 
-        img = Image.open(os.path.join(input_dir, file)).convert("RGB")
-        canny_map = prepare_canny_image(img, THREE_CATS_CANNY_VALUES)
+    style_prompts = 'in line art style'
+    reference_prompts = 'An ink painting'
+    main_prompts = 'A photo of a sitting cat'
 
-        images, stats, layer_names = model.call(prompts,
-                                                image=canny_map,
-                                                num_inference_steps=50,
-                                                num_images_per_prompt=1,
-                                                controlnet_conditioning_scale=0.8,
-                                                guidance_scale=5
-                                                )
-        
-        # save images and stats
-        images[0].save(os.path.join(output_style_dir, f"style_{k}.png"))
-        images[1].save(os.path.join(output_image_dir, f"gen_{k}_{j}.png"))
-        canny_map.save(os.path.join(output_canny_dir, f"canny_{k}_{j}.png"))
-        torch.save(stats, os.path.join(output_stats_dir, f"stats_{i}_{j}"))
+    target_prompt = f"{main_prompts}, {style_prompts}"
+    reference_prompt = f"{reference_prompts}, {style_prompts}"
+    prompts = [reference_prompt, target_prompt]
 
-        if not os.path.exists(layer_names_file):
-            torch.save(layer_names, layer_names_file)
+    files = os.listdir(input_dir)
 
+    style_dict = {artist: sorted([f_name for f_name in files if artist in f_name]) for artist in set([f_name.split("_")[0] for f_name in files])}
+    num_styles = len(style_dict.keys())
+    num_per_style = len(list(style_dict.values())[0])
+
+
+    for i, k in enumerate(style_dict.keys()):
+        assert len(style_dict[k]) == num_per_style
+        for j, file in enumerate(style_dict[k]):
+            torch.manual_seed(10)
+
+            img = Image.open(os.path.join(input_dir, file)).convert("RGB")
+            canny_map = prepare_canny_image(img, THREE_CATS_CANNY_VALUES)
+
+            images, stats, layer_names = model.call(prompts,
+                                                    image=canny_map,
+                                                    num_inference_steps=50,
+                                                    num_images_per_prompt=1,
+                                                    controlnet_conditioning_scale=0.8,
+                                                    guidance_scale=5
+                                                    )
             
+            # save images and stats
+            images[0].save(os.path.join(output_style_dir, f"style_{k}.png"))
+            images[1].save(os.path.join(output_image_dir, f"gen_{k}_{j}.png"))
+            canny_map.save(os.path.join(output_canny_dir, f"canny_{k}_{j}.png"))
+            torch.save(stats, os.path.join(output_stats_dir, f"stats_{i}_{j}"))
+
+            if not os.path.exists(layer_names_file):
+                torch.save(layer_names, layer_names_file)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--series_name", type=str, default=SERIES_NAME)
+    parser.add_argument("--subject", type=str, default=SUBJECT)
+    args = parser.parse_args()
+    main(args)
